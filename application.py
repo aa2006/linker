cb838420c54b287ad77e1adc45f289f7a96454db
Adam Salhi <adam06genius@gmail.com>
2021-07-29 19:40:33 +0000

    Can register/login

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from functions import apology, login_required
+from uri import db
 
 app = Flask(__name__)
 
@@ -28,6 +29,44 @@ Session(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=username)
+        if len(rows) != 0:
+            return apology("username already taken.")
+
+        rows = db.execute("SELECT * FROM users WHERE email = :email", email=email)
+        if len(rows) != 0:
+            return apology("email already taken.")
+        
+        db.execute("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)", username=username, email=email, password=generate_password_hash(password))
+
+        user_id = db.execute("SELECT * FROM users WHERE username = :username", username=username)[0]["id"]
+        session["user_id"] = user_id
+
+        return redirect("/")
 
 def errorhandler(e):
     """Handle error"""
